e20e15c1c319508151953d617d502a0732e43410
ori danus <oridanus@gmail.com>
2016-02-27 23:44:53 +0200

    Added files via upload

---
+++ copy-episode.py
@@ -0,0 +1,25 @@
+import PTN
+import os
+import sys
+import shutil
+from time import gmtime, strftime
+
+log_file = open('/tmp/copy-episode.log', 'a')
+log_file.write('======START========= ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '===================\n')
+episode_dir = sys.argv[1]
+log_file.write('Episode Dir: ' + episode_dir + "\n")
+
+episode_full_path = os.path.join(episode_dir, [f for f in os.listdir(episode_dir) if f.endswith('.mkv')][0])
+print episode_full_path
+base_series_dest_dir = sys.argv[2]
+log_file.write('Base Series Dest Dir: ' + base_series_dest_dir + "\n")
+
+episode_file = os.path.basename(episode_full_path)
+parse_results = PTN.parse(episode_file)
+dest_path = os.path.join(base_series_dest_dir, parse_results['title'], 'Season ' + str(parse_results['season']), episode_file)
+
+log_file.write('Copying ' + episode_full_path + ' To: ' + dest_path + '\n') 
+shutil.copyfile(episode_full_path , dest_path)
+log_file.write('=======DONE======== ' + strftime("%Y-%m-%d %H:%M:%S", gmtime()) + '===================\n')
+
+log_file.close()
